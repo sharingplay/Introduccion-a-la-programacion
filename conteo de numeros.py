c09c3ff60b134c6dd2aee33aa8dcad5c61a8df98
sharingplay <36578275+sharingplay@users.noreply.github.com>
2018-02-28 14:36:37 -0600

    Add files via upload

---
+++ conteo de numeros.py
@@ -0,0 +1,15 @@
+#Dado un numero, determine su longitud (numero de digitos)
+#Entrada: es un numero entero
+#Restricciones: es un entero positivo mayor a cero
+#Salida: longitud de un numero
+def cantidad_digitos(num):
+    if isinstance (num, int) and (num > 0):
+        return cantidad_digitos_total (abs (num))
+    else:
+        return "Error"
+def cantidad_digitos_total (num):
+    if (num == 0):
+        return 0
+    else:
+        return 1 + cantidad_digitos_total (num // 10)
+    
