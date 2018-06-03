793d9769b46e217d66b2071282f969b3382e6055
sharingplay <36578275+sharingplay@users.noreply.github.com>
2018-06-02 22:42:46 -0600

    Add files via upload

---
+++ Python/Iteracion/SumarLista.py
@@ -0,0 +1,10 @@
+class SumarLista:
+    def __init__(self):
+        pass
+    def sumar(lista):
+        suma = 0
+        for elemento in lista:
+            suma += elemento
+        return suma
+            
+            
